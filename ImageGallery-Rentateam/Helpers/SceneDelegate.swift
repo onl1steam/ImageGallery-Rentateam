86a6ab83af04f3cb7abae0e4c06620c0ddd83e2a
Artem Ryzhkov (intern) <onl1steam@gmail.com>
2021-02-11 15:59:32 +0300

    Настройка проекта

---
+++ ImageGallery-Rentateam/Helpers/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  ImageGallery-Rentateam
+//
+//  Created by Рыжков Артем on 11.02.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let initialViewController = UIViewController()
+        let navigationController = UINavigationController(rootViewController: initialViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}

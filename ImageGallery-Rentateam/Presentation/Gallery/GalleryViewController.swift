bbc31b1fe444bb9b3293e62cce684aa904f8c619
Artem Ryzhkov (intern) <onl1steam@gmail.com>
2021-02-12 13:20:20 +0300

    Добавлен вывод ячеек с изображениями

--- ImageGallery-Rentateam/Presentation/Gallery/GalleryViewController.swift
+++ ImageGallery-Rentateam/Presentation/Gallery/GalleryViewController.swift
@@ -24,7 +24,22 @@ class GalleryViewController: UIViewController {
         
         return collection
     }()
-
+    
+    private let galleryService: GalleryServiceProtocol
+    private let imageService: ImageServiceProtocol
+    private var data: [GalleryItem] = []
+    
+    init(galleryService: GalleryServiceProtocol = GalleryService(),
+         imageService: ImageServiceProtocol = ImageService()) {
+        self.galleryService = galleryService
+        self.imageService = imageService
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -33,15 +48,13 @@ class GalleryViewController: UIViewController {
         collectionView.dataSource = self
         
         setupCollectionViewPosition()
-        
-        let galleryService: GalleryServiceProtocol = GalleryService()
         let urlString = "https://api.unsplash.com/photos/?client_id=6g-UBx5WMaXIWFxJVdVTdZ0XjOJbyflcYFsAUFlkRXs&page=1"
-        galleryService.fetchGallery(urlString: urlString) { response in
+        galleryService.fetchGallery(urlString: urlString) { [weak self] response in
             switch response {
             case .success(let json):
                 do {
-                    let galleryItems = try JSONDecoder().decode([GalleryItem].self, from: json)
-                    print("Items: \(galleryItems)")
+                    self?.data = try JSONDecoder().decode([GalleryItem].self, from: json)
+                    self?.collectionView.reloadData()
                 } catch let error {
                     print("Error: \(error.localizedDescription)")
                 }
@@ -68,10 +81,30 @@ extension GalleryViewController: UICollectionViewDelegate {}
 extension GalleryViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return data.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.reuseIdentifier,
+                                                            for: indexPath) as? ImageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.setLabel(data[indexPath.row].imageDescription)
+        cell.dataTask = imageService.fetchImage(urlString: data[indexPath.row].urls.regular, completion: { response in
+                switch response {
+                case .success(let data):
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        cell.setImage(image)
+                    }
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        print("Image loading failed: \(error.localizedDescription)")
+                    }
+                }
+            }
+        )
+        return cell
     }
 }
--- ImageGallery-Rentateam/Presentation/Gallery/ImageCell/ImageCollectionViewCell.swift
+++ ImageGallery-Rentateam/Presentation/Gallery/ImageCell/ImageCollectionViewCell.swift
@@ -8,5 +8,88 @@
 import UIKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
+    
+    private enum CellConstraints {
+        static let imageViewHeight: CGFloat = 150
+        static let imageViewWidth: CGFloat = 150
+    }
+    
     static let reuseIdentifier = String(describing: ImageCollectionViewCell.self)
+    var dataTask: URLSessionDataTask?
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "placeholder")
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addCellSubviews()
+        setupCellConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UICollectionViewCell
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        dataTask?.cancel()
+    }
+    
+    // MARK: Public Methods
+    
+    func setImage(_ image: UIImage?) {
+        imageView.image = image
+    }
+    
+    func setLabel(_ text: String) {
+        titleLabel.text = text
+    }
+    
+    // MARK: Private Methods
+    
+    private func addCellSubviews() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+    }
+    
+    private func setupCellConstraints() {
+        setupImageViewPositionConstraints()
+        setupTitleLabelConstraints()
+    }
+    
+    private func setupImageViewPositionConstraints() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalToConstant: CellConstraints.imageViewHeight),
+            imageView.widthAnchor.constraint(equalToConstant: CellConstraints.imageViewWidth),
+            imageView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
+    
+    private func setupTitleLabelConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor)
+        ])
+    }
 }
